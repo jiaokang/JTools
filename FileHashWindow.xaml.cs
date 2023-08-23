b53caecb03beb1095386c1a200f4897efc983c70
jiaokang <jiaoikang@gmail.com>
2023-08-23 18:26:30 +0800

    新增获取文件hash值功能

--- FileHashWindow.xaml.cs
+++ FileHashWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Windows;
 
 namespace JTools
@@ -19,7 +22,31 @@ namespace JTools
             if (openFileDialog.ShowDialog() == true)
             {
                 string filePath = openFileDialog.FileName;
+                string hashString = Md5Digest(filePath);
+                output_box.Text = hashString;
             }
         }
+
+        private static string Md5Digest(string filePath)
+        {
+            byte[] digestByte;
+            using (MD5 md5 = MD5.Create())
+            {
+                using (FileStream fileStream = File.OpenRead(filePath))
+                {
+                    digestByte = md5.ComputeHash(fileStream);
+                }
+            }
+            string hashString = BitConverter.ToString(digestByte).Replace("-", string.Empty);
+            return hashString;
+        }
+
+        private void copy_clipboard_Click(object sender, RoutedEventArgs e)
+        {
+            string md5DigestString = output_box.Text;
+            Clipboard.SetText(md5DigestString);
+            MessageBox.Show("内容已复制到剪切板");
+        }
+
     }
 }
