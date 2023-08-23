070a121272fb03642c126a5c7f07415e728590ff
jiaokang <jiaoikang@gmail.com>
2023-08-23 17:42:26 +0800

    打开选择文件窗口

--- FileHashWindow.xaml.cs
+++ FileHashWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace JTools
 {
@@ -22,6 +11,15 @@ namespace JTools
         public FileHashWindow()
         {
             InitializeComponent();
+        }
+
+        private void open_file_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+            }
         }
     }
 }
